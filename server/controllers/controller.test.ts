5b6e50e717fc54c660404c6869d44950d5adef3d
AxAnder0095 <doriscafe@hotmail.com>
2026-03-14 11:12:12 -0500

    debugging server

--- server/controllers/controller.test.ts
+++
@@ -1,7 +0,0 @@
-export const getTestStatus = (res: any, req: any) => {
-    try {
-        res.status(200).json({ message: 'Test route is working!' });
-    }catch (error) {
-        res.status(500).json({ message: 'Server error' });
-    }
-};
--- server/routes/test.route.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getTestStatus } from '../controllers/controller.test';
-
-const router = express.Router();
-
-router.get('/test', getTestStatus);
-
-export default router;
